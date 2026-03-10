22e56a86ab15798ea768cb824c4220e6df562207
Kudzai P Matizirofa <kudzaiprichard@gmail.com>
2026-03-10 20:11:29 +0200

    chore(simulations): update internal module exports
    - Add parse_and_validate_csv to __all__ exports
    - Explicitly list all public symbols for consistency

--- src/modules/simulations/internal/__init__.py
+++ src/modules/simulations/internal/__init__.py
@@ -1,4 +1,13 @@
 from src.modules.simulations.internal.reward_oracle import reward_oracle
-from src.modules.simulations.internal.simulation_runner import run_simulation, simulation_registry
+from src.modules.simulations.internal.simulation_runner import (
+    run_simulation,
+    parse_and_validate_csv,
+    simulation_registry,
+)
 
-__all__ = ["reward_oracle", "run_simulation", "simulation_registry"]
+__all__ = [
+    "reward_oracle",
+    "run_simulation",
+    "parse_and_validate_csv",
+    "simulation_registry",
+]
